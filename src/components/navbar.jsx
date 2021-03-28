49cc27b416d88c91d5e6c654a2d49193c6198448
Ismayil Tahmazov <tahmazov.ismayil@gmail.com>
2021-03-28 16:47:27 +0400

    commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, {  } from 'react';
+
+const NavBar = ({totalCounters}) => {
+    return (
+       <nav className="navbar navbar-light bg-light">
+       <div className="container-fluid">
+         <a className="navbar-brand" href="http://localhost:3000/">
+             Navbar
+              <span className="badge badge-pill badge-secondary m-2">
+                  {totalCounters}
+             </span>
+             </a>
+       </div>
+     </nav>);
+   };
+
+ 
+export default NavBar;
